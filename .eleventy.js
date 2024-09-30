07540366f0a4fb8d26595d2583556d73dfe1e5ea
Sravan Reddam <reddamsravan@gmail.com>
2024-10-01 00:29:51 +0530

    chore: setup

---
+++ .eleventy.js
@@ -0,0 +1,27 @@
+const tailwind = require("tailwindcss");
+const postcss = require("postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+
+const postcssFilter = (css, done) => {
+    postcss([
+        tailwind(require("./tailwind.config")),
+        autoprefixer(),
+        cssnano({ preset: "default" }),
+    ])
+        .process(css, {
+            from: "./_includes/css/tailwind.css",
+        })
+        .then(
+            (result) => done(null, result.css),
+            (error) => done(error, null)
+        );
+};
+
+module.exports = function (config) {
+    config.addPassthroughCopy("favicon.svg");
+    config.setServerPassthroughCopyBehavior("passthrough");
+
+    config.addWatchTarget("./_includes/css/tailwind.css");
+    config.addNunjucksAsyncFilter("postcss", postcssFilter);
+};
